8c10c0d85ccf116d86d462a0be399ae9400de21b
Héctor Chocobar-Torrejón <45291267+hchocobar@users.noreply.github.com>
2025-02-21 19:45:48 -0300

    Revert "[fix] pep8"

--- src/datastructures.py
+++ src/datastructures.py
@@ -1,5 +1,6 @@
+
 """
-Update this file to implement the following already declared methods:
+update this file to implement the following already declared methods:
 - add_member: Should add a member to the self._members list
 - delete_member: Should delete a member from the self._members list
 - update_member: Should update a member from the self._members list
@@ -7,19 +8,19 @@ Update this file to implement the following already declared methods:
 """
 from random import randint
 
-
 class FamilyStructure:
-
     def __init__(self, last_name):
         self.last_name = last_name
-        self._members = []  # Example list of members
 
-    # Read-only: Use this method to generate random members ID's when adding members into the list
-    def _generate_id(self):
+        # example list of members
+        self._members = []
+
+    # read-only: Use this method to generate random members ID's when adding members into the list
+    def _generateId(self):
         return randint(0, 99999999)
 
     def add_member(self, member):
-        # Fill this method and update the return
+        # fill this method and update the return
         pass
 
     def delete_member(self, id):
@@ -27,9 +28,9 @@ class FamilyStructure:
         pass
 
     def get_member(self, id):
-        # Fill this method and update the return
+        # fill this method and update the return
         pass
 
-    # This method is done, it returns a list with all the family members
+    # this method is done, it returns a list with all the family members
     def get_all_members(self):
         return self._members
